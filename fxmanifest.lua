69baaf2ecd2f294283a13cd6cec241228c376615
Isaac Mason <40875081+Eyesmack@users.noreply.github.com>
2025-07-28 19:48:27 +1200

    Changed the file structure
    Changed the file structure so that you can add and remove client scripts from the client folder without needing to change the fxmanifest file. This means you can pick and choose what scripts of mine to include in your server.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,13 +2,14 @@ fx_version "cerulean"
 game "gta5"
 
 author "Isaac"
-version "0.2.0"
+version "0.2.1"
 description "Isaacs Scripts - A collection of scripts for FiveM."
 lua54 "yes"
 
-server_script 'server.lua'
+server_scripts {
+    'version_checker.lua'
+}
 
 client_scripts {
-    'simplecarcommands.lua',
-    'notify.lua'
+    'client/*.lua'
 }
