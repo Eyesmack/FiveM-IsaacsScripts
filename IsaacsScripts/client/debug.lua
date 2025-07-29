edb8681caa80c8bf604c5a8c80b81bfd60390c0b
Isaac Mason <40875081+Eyesmack@users.noreply.github.com>
2025-07-30 02:15:24 +1200

    Add commands to control ped and traffic density
    Introduced 'ped' and 'traffic' commands in debug.lua to dynamically adjust pedestrian and vehicle density in-game for testing purposes. Updated fxmanifest.lua version to 1.4.0 to reflect new functionality.

--- IsaacsScripts/client/debug.lua
+++ IsaacsScripts/client/debug.lua
@@ -1,2 +1,43 @@
 -- A script for all my debugging and testing needs.
+local pedFrequency = 1.0 -- 1.0 is normal, 0.0 is no peds
+local trafficFrequency = 1.0 -- 1.0 is normal, 0.0 is no traffic
 
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        SetPedDensityMultiplierThisFrame(pedFrequency) -- https://runtime.fivem.net/doc/natives/#_0x95E3D6257B166CF2
+        SetScenarioPedDensityMultiplierThisFrame(pedFrequency, pedFrequency) -- https://runtime.fivem.net/doc/natives/#_0x7A556143A1C03898
+
+        SetRandomVehicleDensityMultiplierThisFrame(trafficFrequency) -- https://runtime.fivem.net/doc/natives/#_0xB3B3359379FE77D3
+        SetParkedVehicleDensityMultiplierThisFrame(trafficFrequency) -- https://runtime.fivem.net/doc/natives/#_0xEAE6DCC7EEE3DB1D
+        SetVehicleDensityMultiplierThisFrame(trafficFrequency) -- https://runtime.fivem.net/doc/natives/#_0x245A6883D966D537
+    end 
+end)
+
+RegisterCommand('ped', function (source, args, rawCommand)
+    if #args > 0 then
+        local frequency = tonumber(args[1])
+        if frequency ~= nil and frequency >= 0.0 and frequency <= 1.0 then
+            pedFrequency = frequency
+            TriggerEvent('notify:cl:notify', "Ped density set to " .. pedFrequency)
+        else
+            TriggerEvent('notify:cl:notify', "Invalid ped density value. Must be between 0.0 and 1.0.")
+        end
+    else
+        TriggerEvent('notify:cl:notify', "Current ped density: " .. pedFrequency)
+    end
+end, false)
+
+RegisterCommand('traffic', function (source, args, rawCommand)
+    if #args > 0 then
+        local frequency = tonumber(args[1])
+        if frequency ~= nil and frequency >= 0.0 and frequency <= 1.0 then
+            trafficFrequency = frequency
+            TriggerEvent('notify:cl:notify', "Ped density set to " .. trafficFrequency)
+        else
+            TriggerEvent('notify:cl:notify', "Invalid ped density value. Must be between 0.0 and 1.0.")
+        end
+    else
+        TriggerEvent('notify:cl:notify', "Current ped density: " .. trafficFrequency)
+    end
+end, false)
